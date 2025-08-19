4d8d396cffb77687f1e0824eed368024b3a7be65
kazuya <sbvfkku0985@gmail.com>
2025-08-19 20:42:41 +0900

    feat: 本番用のパスワード再設定機能を実装

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,4 @@
 class UserMailer < ApplicationMailer
-  default from: "no-reply@example.com"
-
   def reset_password(user)
     @user = user
     @url  = edit_password_reset_url(@user.reset_password_token) # トークンを path param で
