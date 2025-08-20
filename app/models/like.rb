9c3235c57f00a97a2a931ba7e24c37d3dade936e
kazuya <sbvfkku0985@gmail.com>
2025-08-21 08:38:47 +0900

    feat: Like/UsedCode モデルとマイグレーションを追加（ユニーク制約・FK・counter_cache 連携）

---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+# app/models/like.rb
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :pre_code, counter_cache: :like_count
+
+  validates :user_id, uniqueness: { scope: :pre_code_id }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # ---------- アソシエーション ----------
   has_many :pre_codes, dependent: :destroy
+  has_many :likes,      dependent: :destroy
+  has_many :used_codes, dependent: :destroy
 
   # ---------- 正規化 ----------
   before_validation :normalize_email
