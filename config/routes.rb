3db6431f3ee4b29a9c7d627d86dd2ecb97ca1698
kazuya <sbvfkku0985@gmail.com>
2025-10-01 11:40:24 +0900

    教本連動クイズMVPを実装：3テーブル追加、一般回答フロー/結果集計、管理CRUD、最小ビューとRSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :quizzes do
+    namespace :sections do
+      get "questions/show"
+    end
+    get "sections/index"
+    get "sections/show"
+  end
+  get "quizzes/index"
+  get "quizzes/show"
   # 静的ページ
   get "help",    to: "static_pages#help"
   get "terms",   to: "static_pages#terms"
@@ -53,14 +62,50 @@ Rails.application.routes.draw do
     resources :sections, only: :show, controller: :book_sections
   end
 
+  # クイズ機能
+  resources :quizzes, only: %i[index show] do
+    resources :sections, only: %i[index show], module: :quizzes do
+      resources :questions, only: %i[show], module: :sections do
+        post :answer, on: :member
+        get  :answer_page, on: :member
+      end
+      get :result, on: :member
+    end
+  end
+
   # 管理画面
   namespace :admin do
+    get "quiz_questions/index"
+    get "quiz_questions/show"
+    get "quiz_questions/new"
+    get "quiz_questions/create"
+    get "quiz_questions/edit"
+    get "quiz_questions/update"
+    get "quiz_questions/destroy"
+    get "quiz_sections/index"
+    get "quiz_sections/show"
+    get "quiz_sections/new"
+    get "quiz_sections/create"
+    get "quiz_sections/edit"
+    get "quiz_sections/update"
+    get "quiz_sections/destroy"
+    get "quizzes/index"
+    get "quizzes/show"
+    get "quizzes/new"
+    get "quizzes/create"
+    get "quizzes/edit"
+    get "quizzes/update"
+    get "quizzes/destroy"
     root "dashboards#index"
 
     resource  :session,   only: %i[new create destroy]
     resources :books
     resources :book_sections, except: %i[show]
     resources :pre_codes, only: %i[index show edit update destroy]
+    resources :quizzes
+    resources :quiz_sections
+    resources :quiz_questions
+
     resources :users, only: [ :index, :destroy ] do
       member do
         patch :toggle_editor
